42598c45b82c4b839bfd31801a50a69e423b3635
Jack Edwards <jackedwards@outlook.com>
2015-10-10 00:42:19 +0100

    Made m_root const again to prevent segfault.

--- main.cpp
+++ main.cpp
@@ -9,16 +9,6 @@ int main()
 	for (XMLTag* tag : tags)
 		std::cout << "X: " << tag->m_attributes["x"].ToInt() << " Y: " << tag->m_attributes["y"].ToInt() << '\n';
 
-	/*XMLFile file;
-	file.Load("data2.xml");
-	XMLTag* root = file.m_root->GetFirstChild("Resources");
-	std::vector<XMLTag*> tags = root->GetChildren("Texture");
-
-	for (XMLTag* tag : tags)
-	{
-		std::cout << "Path: " << tag->m_attributes["path"].ToString() << "Name: " << tag->m_attributes["name"].ToString() << '\n';
-	}*/
-
 	#ifdef _WIN32
 		system("pause");
 	#endif
